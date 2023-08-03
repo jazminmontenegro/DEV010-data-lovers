b5777961c5b3f0e15280175ae7e07ed48c306048
jazmin_montenegro <jazmin220906@gmail.com>
2023-08-03 12:23:06 -0500

    primera version

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/got/got.js';
 
-console.log(example, data);
+
+const container= document.querySelector('.card')
+for (let i=0 ; i<data.got.length; i++){
+  //console.log(data.got[i]);
+  container.innerHTML += `<figure>
+  <img src=${data.got[i].imageUrl}>
+  <figcaption>${data.got[i].firstName}</figcaption>
+  </figure>`
+}
